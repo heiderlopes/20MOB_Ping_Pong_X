22e8dec8ab51dc3b43c8300c44aa078316a10494
Heider Lopes <heidertreinamentos@gmail.com>
2020-09-01 23:10:25 -0300

    Implementação das activities

---
+++ app/src/main/java/br/com/heiderlopes/pingpongx/Constants.kt
@@ -0,0 +1,8 @@
+package br.com.heiderlopes.pingpongx
+
+object Constants {
+
+    val KEY_EXTRA_PLAYER_1 = "KEY_EXTRA_PLAYER_1"
+    val KEY_EXTRA_PLAYER_2 = "KEY_EXTRA_PLAYER_2"
+
+}
--- app/src/main/java/br/com/heiderlopes/pingpongx/MainActivity.kt
+++ app/src/main/java/br/com/heiderlopes/pingpongx/MainActivity.kt
@@ -1,12 +1,122 @@
 package br.com.heiderlopes.pingpongx
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.os.bundleOf
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var playerOneScore = 0
+    private var playerTwoScore = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("ESTADO", "onCreate")
+
+        if(savedInstanceState != null) {
+            playerOneScore = savedInstanceState.getInt("playerOneScore", 0)
+            playerTwoScore = savedInstanceState.getInt("playerTwoScore", 0)
+            setUpScorePlayerOne()
+            setUpScorePlayerTwo()
+        }
+
+        setUpPlayers()
+        setUpListeners()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("playerOneScore", playerOneScore)
+        outState.putInt("playerTwoScore", playerTwoScore)
+    }
+
+    private fun setUpListeners() {
+        btPlayerOneScore.setOnClickListener {
+            playerOneScore++
+            setUpScorePlayerOne()
+        }
+
+        btPlayerTwoScore.setOnClickListener {
+            playerTwoScore++
+            setUpScorePlayerTwo()
+        }
+
+        btRevenge.setOnClickListener {
+            rematch()
+        }
+
+        btFinishMatch.setOnClickListener {
+            val telaPlayer = Intent(this, PlayerActivity::class.java)
+            telaPlayer.putExtra("rematch", true)
+            startActivity(telaPlayer)
+            finish()
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        intent = data
+        setUpPlayers()
+        rematch()
+    }
+
+
+
+    private fun setUpScorePlayerOne() {
+        tvPlayerOneScore.text = playerOneScore.toString()
+    }
+
+    private fun setUpScorePlayerTwo() {
+        tvPlayerTwoScore.text = playerTwoScore.toString()
+    }
+
+    private fun setUpPlayers() {
+        tvPlayerOneName.text = intent.getStringExtra(Constants.KEY_EXTRA_PLAYER_1)
+        tvPlayerTwoName.text = intent.getStringExtra(Constants.KEY_EXTRA_PLAYER_2)
+    }
+
+    private fun rematch() {
+        playerOneScore = 0
+        playerTwoScore = 0
+        setUpScorePlayerOne()
+        setUpScorePlayerTwo()
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("ESTADO", "onStart")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("ESTADO", "onRestart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("ESTADO", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("ESTADO", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("ESTADO", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("ESTADO", "onDestroy")
     }
 }
